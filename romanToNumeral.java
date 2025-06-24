b10f0bd9c81be385bcc2c782d480fe1ded9c5fcc
ericspencer00 <espencer2@luc.edu>
2025-06-23 21:25:09 -0500

    init

---
+++ romanToNumeral.java
@@ -0,0 +1,33 @@
+public class romanToNumeral {
+    public static int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int value = getValue(c);
+
+            if (value < prevValue) {
+                total -= value;
+            } else {
+                total += value;
+            }
+            prevValue = value;
+        }
+
+        return total;
+    }
+
+    private static int getValue(char c) {
+        return switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        }; // Invalid character
+    }
+}
