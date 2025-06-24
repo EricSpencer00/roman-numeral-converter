acfae2d7a09891e5aa374a4e2b8fea352cdd8ce6
ericspencer00 <espencer2@luc.edu>
2025-06-23 21:36:16 -0500

    Update Main.java to include exit option for Roman numeral input

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@ import romanToNumeral.romanToNumeral;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a Roman numeral: ");
+        System.out.print("Enter a Roman numeral, enter \"exit\" to quit : ");
         romanToNumeral converter = new romanToNumeral();
 
         for (String line; scanner.hasNextLine(); ) {
             line = scanner.nextLine().trim();
-            if (line.isEmpty()) {
+            if (line.isEmpty() || line.equalsIgnoreCase("exit")) {
                 break; // Exit on empty line
             }
             try {
